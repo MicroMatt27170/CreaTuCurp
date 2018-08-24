a17d5077099f05249fba74680707dc54fdb7bb7e
Matthew Montes de Oca <mmmatthew1999@outlook.com>
2018-08-24 13:15:50 -0500

    Initial commit

---
+++ app/src/main/java/com/matthew/micromatt/creatucurp/CrearCurp.java
@@ -0,0 +1,13 @@
+package com.matthew.micromatt.creatucurp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearCurp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_curp);
+    }
+}
